ca91e339079dfa5375bbb49c8bf2ce090d1b47b8
DhrumiPrajapati <86350716+DhrumiPrajapati@users.noreply.github.com>
2023-09-12 12:14:48 +0000

    learned datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version //good practice
+
+// alert(3+3) //we are using nodeJS not browser, so it won't work
+
+// console.log(3+3); console.log("Dhrumi") //do not have code readability, code readability should be high
+
+// console.log(3+
+//     3)                      //do not have code readability, code readability should be high
+
+let name = "Dhrumi"
+let age = 23
+let isLoggedIn = false
+
+// Primitive Datatypes
+// number=> 2 to power of 53 (have range)
+// bigint=> for big numbers
+// string=> " ", ' '
+// boolean=> true/flase
+// null=> standalone value, it is a representation of empty value, it is not similar to undefined
+// undefined=> when var is defined but value is not defined
+// symbol=> unique
+// object
+
+console.log(typeof "Dhrumi");
+console.log(typeof age);
+console.log(typeof null); //object=> weakness of JS
+console.log(typeof undefined); //undefined
